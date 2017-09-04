ca317de72ecdfed6d3b6255c8a711b9d414d13aa
Dim <skyuphigh01@gmail.com>
2017-09-04 15:33:51 -0400

    added @whateverthesearecalled and changed to snekfetch cause smaller

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
-const fetch = require('node-fetch');
+const fetch = require('snekfetch').get;
 const { version } = require('./package.json');
 const appReg = /^\d+$/;
 const idReg = /^\d{17}$/;
 
-class API {
-  constructor(data) {
-    if (!data) console.log('no key provided | some methods may not work | go get one > https://goo.gl/DfNy5s');
+class SteamAPI {
+  /**
+   * Sets Steam key for future use
+   * @param {string} key Steam key
+   */
+  constructor(key) {
+    if (!key) console.log('no key provided | some methods may not work | go get one > https://goo.gl/DfNy5s');
     this.baseURL = 'https://api.steampowered.com';
-    this.key = data;
+    this.key = key;
     this.headers = {
       'User-Agent': `SteamAPI/${version} https://github.com/lloti/steampowered-api`
     };
   }
+  /**
+   * Get custom path that isn't in SteamAPI
+   * @param {string} path Path to request e.g '/IPlayerService/GetOwnedGames/v1?steamid=76561198378422474'
+   * @returns {Promise<object>} JSON Response
+   */
   get(path) {
     return new Promise((resolve, reject) => {
       fetch(`${this.baseURL}${path}${path.includes('?') ? '&' : '?'}key=${this.key}`, this.headers)
-        .then(res => res.json())
-        .then(resolve)
+        .then(res => resolve(res.body))
         .catch(reject);
     });
   }
+  /**
+   * Resolve info based on id|profile|url
+   * @param {string} info Something to resolve e.g 'https://steamcommunity.com/id/xDim'
+   * @returns {Promise<string>} Profile ID
+   */
   resolve(info) {
     if (!info) throw new Error('no info provided');
-    return new Promise(async(resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       let steamID, steamURL;
       if (/^(?:\/?profiles\/)?\d{17}$/.test(info)) {
         steamID = info.replace(/^(?:\/?profiles\/)?(\d{17})$/, '$1');
@@ -42,68 +55,98 @@ class API {
       }
       if (steamURL) {
         steamID = await fetch(`${this.baseURL}/ISteamUser/ResolveVanityURL/v1?key=${this.key}&vanityurl=${steamURL}`, this.headers)
-          .then(res => res.json())
-          .then(json => json.response.success === 1 ? json.response.steamid : reject('Invalid profile link/id'))
+          .then(res => res.body.response.success === 1 ? res.body.response.steamid : reject('Invalid profile link/id'))
           .catch(reject);
       }
       resolve(steamID);
     });
   }
+  /**
+   * Get achievements for app id
+   * @param {string} app App ID
+   * @returns {Promise<Array<string>>} App achievements for ID
+   */
   getGameAchievements(app) {
     if (!appReg.test(app)) throw new Error('no appid provided');
     return new Promise(async (resolve, reject) => {
       const json = await fetch(`${this.baseURL}/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v2?gameid=${app}`, this.headers)
-        .then(res => res.json())
+        .then(res => res.body)
         .catch(reject);
       resolve(json.achievementpercentages.achievements);
     });
   }
+  /**
+   * Get news for app id
+   * @param {string} app App ID
+   * @returns {Promise<Array<Object>} App news for ID
+   */
   getGameNews(app) {
     if (!appReg.test(app)) throw new Error('no appid provided');
     return new Promise(async (resolve, reject) => {
       const json = await fetch(`${this.baseURL}/ISteamNews/GetNewsForApp/v2?appid=${app}`, this.headers)
-        .then(res => res.json())
+        .then(res => res.body)
         .catch(reject);
       if (json.appnews.count === 0) return reject('no news found');
       resolve(json.appnews.newsitems);
     });
   }
+  /**
+   * Get number of current players for app id
+   * @param {string} app App ID
+   * @returns {Promise<number>} Number of players
+   */
   getGamePlayers(app) {
     if (!appReg.test(app)) throw new Error('no appid provided');
     return new Promise(async (resolve, reject) => {
-      const json = await fetch(`${this.baseURL}/ISteamUserStats/GetNumberOfCurrentPlayers/v1?appid=${app}&count=1&name[0]`, this.headers)
-        .then(res => res.json())
+      const json = await fetch(`${this.baseURL}/ISteamUserStats/GetNumberOfCurrentPlayers/v1?appid=${app}`, this.headers)
+        .then(res => res.body)
         .catch(reject);
       if (json.response.result !== 1) return reject('invalid app id');
       resolve(json.response.player_count);
     });
   }
+  /**
+   * Get schema for app id
+   * @param {string} app App ID
+   * @returns {Promise<number>} Schema
+   */
   getGameSchema(app) {
     if (!appReg.test(app)) throw new Error('no appid provided');
     return new Promise(async (resolve, reject) => {
       const json = await fetch(`${this.baseURL}/ISteamUserStats/GetSchemaForGame/v2?key=${this.key}&appid=${app}`, this.headers)
-        .then(res => res.json())
+        .then(res => res.body)
         .catch(reject);
       if (!json.game) return reject('game not found');
       resolve(json.game);
     });
   }
-  getUserAchievements(player, app) {
-    if (!idReg.test(player)) throw new Error('no player id provided');
+  /**
+   * Get users achievements for app id
+   * @param {string} id User ID
+   * @param {string} app App ID
+   * @returns {Promise<Object>} Achievements
+   */
+  getUserAchievements(id, app) {
+    if (!idReg.test(id)) throw new Error('no user id provided');
     if (!/^\d+$/.test(app)) throw new Error('no appid provided');
     return new Promise(async (resolve, reject) => {
-      const json = await fetch(`${this.baseURL}/ISteamUserStats/GetPlayerAchievements/v1?key=${this.key}&steamid=${player}&appid=${app}`, this.headers)
-        .then(res => res.json())
+      const json = await fetch(`${this.baseURL}/ISteamUserStats/GetPlayerAchievements/v1?key=${this.key}&steamid=${id}&appid=${app}`, this.headers)
+        .then(res => res.body)
         .catch(reject);
       if (!json.playerstats.success) return reject('error getting achievements');
       resolve(json.playerstats);
     });
   }
+  /**
+   * Get users bans
+   * @param {string} id User ID
+   * @returns {Promise<Object>} Ban info
+   */
   getUserBans(id) {
     if (!idReg.test(id)) throw new Error('no id provided');
     return new Promise(async (resolve, reject) => {
       const json = await fetch(`${this.baseURL}/ISteamUser/GetPlayerBans/v1?key=${this.key}&steamids=${id}`, this.headers)
-        .then(res => res.json())
+        .then(res => res.body)
         .catch(reject);
       const ban = json.players[0];
       resolve({
@@ -117,11 +160,16 @@ class API {
       });
     });
   }
+  /**
+   * Get users friends
+   * @param {string} id User ID
+   * @returns {Promise<Array<Object>>} Friends
+   */
   getUserFriends(id) {
     if (!idReg.test(id)) throw new Error('no id provided');
     return new Promise(async (resolve, reject) => {
       const json = await fetch(`${this.baseURL}/ISteamUser/GetFriendList/v1?key=${this.key}&steamid=${id}`, this.headers)
-        .then(res => res.json())
+        .then(res => res.body)
         .catch(reject);
       const friends = json.friendslist.friends;
       const newFriends = [];
@@ -135,32 +183,48 @@ class API {
       resolve(newFriends);
     });
   }
+  /**
+   * Get users groups
+   * @param {string} id User ID
+   * @returns {Promise<Array<string>>} Groups
+   */
   getUserGroups(id) {
     if (!idReg.test(id)) throw new Error('no id provided');
     return new Promise(async (resolve, reject) => {
       const json = await fetch(`${this.baseURL}/ISteamUser/GetUserGroupList/v1?key=${this.key}&steamid=${id}`, this.headers)
-        .then(res => res.json())
+        .then(res => res.body)
         .catch(reject);
       if (!json.response.success) return reject('failed');
       resolve(json.response.groups.map(group => group.gid));
     });
   }
-  getUserStats(player, app) {
-    if (!idReg.test(player)) throw new Error('no player id provided');
+  /**
+   * Get users stats for app id
+   * @param {string} id User ID
+   * @param {string} app App ID
+   * @returns {Promise<Object>} Stats for app id
+   */
+  getUserStats(id, app) {
+    if (!idReg.test(id)) throw new Error('no user id provided');
     if (!/^\d+$/.test(app)) throw new Error('no appid provided');
     return new Promise(async (resolve, reject) => {
-      const json = await fetch(`${this.baseURL}/ISteamUserStats/GetUserStatsForGame/v2?key=${this.key}&steamid=${player}&appid=${app}`, this.headers)
-        .then(res => res.json())
+      const json = await fetch(`${this.baseURL}/ISteamUserStats/GetUserStatsForGame/v2?key=${this.key}&steamid=${id}&appid=${app}`, this.headers)
+        .then(res => res.body)
         .catch(reject);
       if (!json.playerstats) return reject('game not found for user');
       resolve(json.playerstats);
     });
   }
+  /**
+   * Get users summary
+   * @param {string} id User ID
+   * @returns {Promise<Object>} Summary
+   */
   getUserSummary(id) {
     if (!idReg.test(id)) throw new Error('no id provided');
     return new Promise(async (resolve, reject) => {
       const json = await fetch(`${this.baseURL}/ISteamUser/GetPlayerSummaries/v2?key=${this.key}&steamids=${id}`, this.headers)
-        .then(res => res.json())
+        .then(res => res.body)
         .catch(reject);
       const player = json.response.players[0];
       if (!player) return reject('invalid id');
@@ -186,4 +250,4 @@ class API {
   }
 }
 
-module.exports = API;
+module.exports = SteamAPI;
--- test/test.js
+++ test/test.js
@@ -4,28 +4,28 @@ const steam = new SteamAPI(key);
 
 const inspect = json => console.log(require('util').inspect(json, { depth: null }));
 
-steam.resolve('xDim').then(async id => {
-    console.log('resolve');
-    console.log(id);
-    console.log('getGameAchievements');
-    await steam.getGameAchievements(730).then(inspect).catch(inspect);
-    console.log('getGameNews');
-    await steam.getGameNews(730).then(inspect).catch(inspect);
-    console.log('getGamePlayers');
-    await steam.getGamePlayers(730).then(inspect).catch(inspect);
-    console.log('getGameSchema');
-    await steam.getGameSchema(730).then(inspect).catch(inspect);
+steam.resolve('DimGG').then(async id => {
+  console.log('resolve');
+  console.log(id);
+  console.log('getGameAchievements');
+  await steam.getGameAchievements(730).then(inspect);
+  console.log('getGameNews');
+  await steam.getGameNews(730).then(inspect);
+  console.log('getGamePlayers');
+  await steam.getGamePlayers(730).then(inspect);
+  console.log('getGameSchema');
+  await steam.getGameSchema(730).then(inspect);
 
-    console.log('getUserAchievements');
-    await steam.getUserAchievements(id, 730).then(inspect);
-    console.log('getUserBans');
-    await steam.getUserBans(id).then(inspect);
-    console.log('getUserFriends');
-    await steam.getUserFriends(id).then(inspect);
-    console.log('getUserGroups');
-    await steam.getUserGroups(id).then(inspect);
-    console.log('getUserStats');
-    await steam.getUserStats(id, 730).then(inspect);
-    console.log('getUserSummary');
-    await steam.getUserSummary(id).then(inspect);
+  console.log('getUserAchievements');
+  await steam.getUserAchievements(id, 730).then(inspect);
+  console.log('getUserBans');
+  await steam.getUserBans(id).then(inspect);
+  console.log('getUserFriends');
+  await steam.getUserFriends(id).then(inspect);
+  console.log('getUserGroups');
+  await steam.getUserGroups(id).then(inspect);
+  console.log('getUserStats');
+  await steam.getUserStats(id, 730).then(inspect);
+  console.log('getUserSummary');
+  await steam.getUserSummary(id).then(inspect);
 }).catch(inspect);
