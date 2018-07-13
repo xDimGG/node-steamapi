1e71c46e0871aca5638a11231290d47589a71504
Falsejoey <11788737+Falsejoey@users.noreply.github.com>
2018-07-13 10:28:14 -0400

    Modification to Game.js
    Removing game.playtime_2week because it no longer returns that with the "include_appinfo=1" tag (there already is a getUserRecentGames(id) anyways), Adding Name, logo and icon urls

--- src/structures/Game.js
+++ src/structures/Game.js
@@ -1,8 +1,12 @@
+const cdn = 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps';
+
 class Game {
 	constructor(game) {
+		this.name = game.name;
 		this.appID = game.appid;
 		this.playTime = game.playtime_forever;
-		this.playTime2 = game.playtime_2weeks || 0;
+		this.logoURL = `${cdn}/${game.appid}/${game.img_logo_url}.jpg`;
+		this.iconURL = `${cdn}/${game.appid}/${game.img_icon_url}.jpg`;
 	}
 }
 
