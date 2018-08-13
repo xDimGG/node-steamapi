12319a4ade96baa8eb50080c108f20d22b244538
Falsejoey <11788737+Falsejoey@users.noreply.github.com>
2018-08-13 08:57:52 -0400

    Fixing a Error
    added back `playtime2` after it was removed by mistake.

--- src/structures/Game.js
+++ src/structures/Game.js
@@ -5,6 +5,7 @@ class Game {
 		this.name = game.name;
 		this.appID = game.appid;
 		this.playTime = game.playtime_forever;
+		this.playTime2 = game.playtime_2weeks || 0;
 		this.logoURL = `${cdn}/${game.appid}/${game.img_logo_url}.jpg`;
 		this.iconURL = `${cdn}/${game.appid}/${game.img_icon_url}.jpg`;
 	}
