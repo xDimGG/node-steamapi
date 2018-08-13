bb98fa3830e0752afcb519353b8fa8b30e2ec965
Tim Watkins <timothy.watkins@gmail.com>
2018-08-12 20:51:12 -0600

    Change cityId to cityID in PlayerSummary

--- src/structures/PlayerSummary.js
+++ src/structures/PlayerSummary.js
@@ -20,7 +20,7 @@ class PlayerSummary extends Player {
 		this.visibilityState = player.communityvisibilitystate;
 		this.countryCode = player.loccountrycode;
 		this.stateCode = player.locstatecode;
-		this.cityId = player.loccityid;
+		this.cityID = player.loccityid;
 	}
 
 	/**
