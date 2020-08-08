a38e262be7ab02b3847126bd064a7f3e3b98e779
pikama <34977908+pikama@users.noreply.github.com>
2020-08-08 22:29:26 +0800

    Added all of the region that Steam provided

--- src/SteamAPI.js
+++ src/SteamAPI.js
@@ -13,7 +13,7 @@ const objectify = require('./utils/objectify');
 const fetch = require('./utils/fetch');
 const { version, name } = require('../package.json');
 const reApp = /^\d{1,7}$/;
-const reRegion = /^us|es|de|fr|ru|nz|au|uk$/i;
+const reRegion = /^af|ax|al|dz|as|ad|ao|ai|aq|ag|ar|am|aw|au|at|az|bl|bq|bs|bh|bd|bb|by|be|bz|bj|bm|bt|bo|ba|bw|bv|br|io|bn|bg|bf|bi|cu|cw|kh|cm|ca|cv|ky|cf|td|cl|cn|cx|cc|co|km|cg|cd|ck|cr|ci|hr|cy|cz|dk|dj|dm|do|ec|eg|sv|gq|er|ee|sz|et|fk|fo|fj|fi|fr|gf|pf|tf|ga|gm|ge|de|gh|gi|gr|gl|gd|gp|gu|gt|gg|gn|gw|gy|ht|hm|va|hn|hk|hu|is|in|id|ir|iq|ie|im|il|it|jm|jp|je|jo|kp|kz|ke|ki|kr|xk|kw|kg|la|lv|lb|ls|lr|ly|li|lt|lu|mf|mo|mk|mg|mw|my|mv|ml|mt|mh|mq|mr|mu|yt|mx|fm|md|mc|mn|me|ms|ma|mz|mm|na|nr|np|nl|nc|nz|ni|ne|ng|nu|nf|mp|no|om|pk|pw|ps|pa|pg|py|pe|ph|pn|pl|pt|pr|qa|re|ro|ru|rw|ss|sx|sh|kn|lc|pm|vc|ws|sm|st|sa|sn|rs|sc|sl|sg|sk|si|sb|so|za|gs|es|lk|sd|sr|sj|se|ch|sy|tw|tj|tz|th|tl|tg|tk|to|tt|tn|tr|tm|tc|tv|ug|ua|ae|gb|us|um|uy|uz|vu|ve|vn|vg|vi|wf|eh|ye|zm|zw$/i;
 const reID = /^\d{17}$/;
 
 const reProfileBase = String.raw`(?:(?:(?:(?:https?)?:\/\/)?(?:www\.)?steamcommunity\.com)?)?\/?`;
