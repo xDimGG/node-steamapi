6c10f3c11348a7aeb0a6d070de017d53910a07c0
Angelo Lloti <angelolloti@Angelos-MacBook-Air.local>
2023-06-14 16:40:52 -0400

    use node-fetch

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,37 +1,13 @@
-const { createDeflate, createGunzip } = require('zlib');
-const { parse } = require('url');
-const https = require('https');
-const http = require('http');
+const fetch = require('node-fetch');
 const reg = /<h1>(.*)<\/h1>/;
 
-module.exports = (url, { headers } = {}) => {
-	const fetch = url.startsWith('https') ? https.get : http.get;
-	const options = Object.assign(
-		parse(url),
-		{ headers: Object.assign({ 'Accept-Encoding': 'gzip, deflate' }, headers) }
-	);
+module.exports = async (url, options) => {
+	const res = await fetch(url, options);
+	if (res.status === 400) {
+		const data = await res.text();
+		throw new Error(reg.test(data) ? data.match(reg)[1] : data);
+	}
+	if (res.status !== 200) throw new Error(res.statusText);
 
-	return new Promise((resolve, reject) => {
-		fetch(options, res => {
-			const ce = res.headers['content-encoding'];
-
-			let data = '';
-			let op = res;
-
-			if (ce === 'deflate') res.pipe(op = createDeflate());
-			if (ce === 'gzip') res.pipe(op = createGunzip());
-
-			op.on('data', chunk => data += chunk);
-			op.once('error', reject);
-			op.once('end', () => {
-				if (res.statusCode === 400) return reject(new Error(reg.test(data) ? data.match(reg)[1] : data));
-				if (res.statusCode !== 200) return reject(new Error(res.statusMessage));
-				try {
-					resolve(JSON.parse(data));
-				} catch (_) {
-					reject(new Error(data));
-				}
-			});
-		});
-	});
+	return res.json();
 };
