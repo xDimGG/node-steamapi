e2d3cef463d1b618d7f8af394c0a7147015f80a6
Angelo Lloti <angelolloti@Angelos-MacBook-Air.local>
2023-06-14 16:38:42 -0400

    fix objectify docs

--- src/utils/objectify.js
+++ src/utils/objectify.js
@@ -1,13 +1,8 @@
 /**
- * Yes, I do know what objectify means.
  * Convert
  * * [{ name: 'a', value: 'b' }, { name: 'b', value: 'c' }]
  * To
  * * { a: 'b', b: 'c' }
- * &
- * * [{ name: 'a', value: 'b', other: 'c' }, { name: 'b', value: 'c', other: 'd' }]
- * To
- * * { a: { value: 'b', other: 'c' }, b: { value: 'c', other: 'd' } }
  * @param {Object[]} arr The array to objectify
  * @param {string} [val=value] The name of the value
  * @param {string} [key=name] The name of the key
