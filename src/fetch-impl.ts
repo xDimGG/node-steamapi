4dd460660869323a6db566ad0602ca8b68655c98
Charles Stello <58380022+SyntaxisCS@users.noreply.github.com>
2025-06-12 17:51:38 -0400

    fix: use native fetch when available, fallback to node-fetch (#67)

---
+++ src/fetch-impl.ts
@@ -0,0 +1,2 @@
+// Uses global fetch if available, otherwise uses node-fetch, allows for support for modern node and serverless environmentsexport 
+export const fetchImpl = typeof fetch !== "undefined" ? fetch : (await import("node-fetch")).default;
